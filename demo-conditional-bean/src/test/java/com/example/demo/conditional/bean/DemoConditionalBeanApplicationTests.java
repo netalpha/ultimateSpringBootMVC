a29e27af73d9c318a93bb48be16b9e1ecd5f2a93
Jay Gong <jay.love.coding@gmail.com>
2020-12-28 14:41:06 +0000

    feat: added example-conditional-bean impl

---
+++ demo-conditional-bean/src/test/java/com/example/demo/conditional/bean/DemoConditionalBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.conditional.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConditionalBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
